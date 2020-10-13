bd6fd9ddd58cbb3401c437604955a5db81cdd703
unknown <mitumariam18@gmail.com>
2020-10-12 21:21:21 -0700

    files stored for homepage component

---
+++ src/homepage.component.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">HATS</h1>
+                    <span className="sub-title">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">jackets</h1>
+                    <span className="sub-title">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Sneakers</h1>
+                    <span className="sub-title">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">women</h1>
+                    <span className="sub-title">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">men</h1>
+                    <span className="sub-title">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+
+export default HomePage;
